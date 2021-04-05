4f6d32c1c8d51b5aec24ddfb026c5c13c9507119
destiny1 <aigbedestinyic@gmail.com>
2021-04-05 12:10:29 +0100

    Done adding the javascript

---
+++ index.js
@@ -0,0 +1,18 @@
+let getNoteId = () => {
+  let noteObject = getExistingNotes();
+  if (!noteObject) {
+    return 1;
+  }
+  const keysArray = Object.keys(noteObject);
+  const numberKeys = keysArray.map((key) => Number(key));
+  console.log(numberKeys);
+  return Math.max(...numberKeys) + 1;
+};
+
+let getExistingNotes = () => {
+  let notes = localStorage.getItem("notes");
+  if (!notes) {
+    return null;
+  }
+  return JSON.parse(notes);
+};
